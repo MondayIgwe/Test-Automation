657bf07d0b8806e728872a36c117534285654814
monday <groovymondee@gmail.com>
2024-12-06 08:51:29 +0200

    testAut 001 : web test set up

--- Web/src/main/java/utils/BrowserDriver.java
+++ Web/src/main/java/utils/BrowserDriver.java
@@ -1,4 +1,43 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class BrowserDriver {
+    private static final ThreadLocal<WebDriver> webDriver = new ThreadLocal<>();
+
+    public static WebDriver setDriver(String browserName) {
+        switch (browserName) {
+            case "chrome":
+                webDriver.set(new ChromeDriver());
+                break;
+            case "firefox":
+                webDriver.set(new FirefoxDriver());
+                break;
+            case "safari":
+                webDriver.set(new SafariDriver());
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browserName);
+        }
+        getDriver().manage().window().maximize();
+        return getDriver();
+    }
+
+    public static WebDriver getDriver() {
+        if (webDriver.get() == null) {
+            throw new IllegalStateException("No WebDriver instance has been set");
+        }
+        return webDriver.get();
+    }
+
+    public static void quitDriver() {
+        if (getDriver() != null) {
+            getDriver().close();
+            getDriver().quit();
+            webDriver.remove();
+        }
+    }
 }
--- Web/src/test/java/stepdefinitions/LoginTest.java
+++ Web/src/test/java/stepdefinitions/LoginTest.java
@@ -1,12 +1,12 @@
 package stepdefinitions;
-import io.cucumber.java.en.*;
+import cucumber.api.java.en.*;
 public class LoginTest {
-    @When("the user logs in with username {string} and password {string}")
-    public void theUserLogsInWithUsernameAndPassword(String arg0, String arg1) {
+    @Given("a user with username john and password secret")
+    public void aUserWithUsernameJohnAndPasswordSecret() {
     }
 
-    @Given("a user with username {string} and password {string}")
-    public void aUserWithUsernameAndPassword(String arg0, String arg1) {
+    @When("the user logs in with username john and password secret")
+    public void theUserLogsInWithUsernameJohnAndPasswordSecret() {
     }
 
     @Then("the user should be successfully logged in")
